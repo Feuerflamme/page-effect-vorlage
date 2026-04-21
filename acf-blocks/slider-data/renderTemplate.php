7347dc2d18d699b8471bc23f3fc56433ee5d1f7b
Thomas Pondelek <thomas.pondelek@page-effect.com>
2026-04-21 13:22:57 +0200

    added slider to data row; deleted image only block

---
+++ acf-blocks/slider-data/renderTemplate.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * Block template file: renderTemplate.php
+ *
+ * Image Data Block Template.
+ *
+ * @param   array $block The block settings and attributes.
+ * @param   string $content The block inner HTML (empty).
+ * @param   bool $is_preview True during AJAX preview.
+ * @param   (int|string) $post_id The post ID this block is saved to.
+ */
+
+// Create id attribute allowing for custom "anchor" value.
+$id = 'image-data-' . $block['id'];
+if (!empty($block['anchor'])) {
+  $id = $block['anchor'];
+}
+
+// Create class attribute allowing for custom "className" and "align" values.
+$classes = 'image-data slider-data';
+if (!empty($block['className'])) {
+  $classes .= ' ' . $block['className'];
+}
+if (!empty($block['align'])) {
+  $classes .= ' align' . $block['align'];
+}
+?>
+
+<style type="text/css">
+  <?php echo '#' . $id;
+
+  ?> {
+    /* Add styles that use ACF values here */
+  }
+</style>
+
+<section id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($classes); ?>">
+  <div class="container">
+
+
+    <div class="image-data__col image-data__col--data">
+      <?php if (have_rows('id::first')): ?>
+        <?php while (have_rows('id::first')):
+          the_row(); ?>
+          <?php $value = get_sub_field('id::value'); ?>
+          <?php $text = get_sub_field('id::text'); ?>
+          <div class="image-data__counter" data-target="<?php echo esc_attr($value); ?>">
+            <div class="image-data__value 
+            <?php the_sub_field('id::color'); ?>">0</div>
+            <?php if ($text): ?>
+              <div class="image-data__text subheadline"><?php echo esc_html($text); ?></div>
+            <?php endif; ?>
+          </div>
+        <?php endwhile; ?>
+      <?php endif; ?>
+    </div>
+    <?php $id_slider = get_field('id::slider'); ?>
+
+    <div class="slider-data image-data__col image-data__col--image">
+      <?php if ($id_slider): ?>
+        <?php echo do_shortcode('[tp_slider id="' . esc_attr($id_slider) . '"]'); ?>
+      <?php endif; ?>
+    </div>
+
+    <div class="image-data__col image-data__col--data">
+      <?php if (have_rows('id::second')): ?>
+        <?php while (have_rows('id::second')):
+          the_row(); ?>
+          <?php $value = get_sub_field('id::value'); ?>
+          <?php $text = get_sub_field('id::text'); ?>
+          <div class="image-data__counter" data-target="<?php echo esc_attr($value); ?>">
+            <div class="image-data__value 
+            <?php the_sub_field('id::color'); ?>">0</div>
+            <?php if ($text): ?>
+              <div class="image-data__text subheadline"><?php echo esc_html($text); ?></div>
+            <?php endif; ?>
+          </div>
+        <?php endwhile; ?>
+      <?php endif; ?>
+    </div>
+  </div>
+</section>
--- functions/acfblock.php
+++ functions/acfblock.php
@@ -24,39 +24,41 @@ class ACFBlock
 new ACFblock("image-text");
 new ACFblock("text");
 new ACFblock("hero");
-new ACFblock("image");
-new ACFblock("image-data");
+new ACFblock("slider-data");
 new acfblock(name: "ansprechpartner");
 new acfblock("kacheln-leistungen");
 
 /**
  * Add custom styles to WYSIWYG editor for better visual editing
  */
-function add_wysiwyg_editor_styles() {
-    add_editor_style('editor-style.css');
+function add_wysiwyg_editor_styles()
+{
+  add_editor_style('editor-style.css');
 }
 add_action('after_setup_theme', 'add_wysiwyg_editor_styles');
 
 /**
  * Enqueue styles for Gutenberg editor (Block Editor)
  */
-function add_gutenberg_editor_styles() {
-    wp_enqueue_style(
-        'theme-editor-styles',
-        get_theme_file_uri('editor-style.css'),
-        array(),
-        wp_get_theme()->get('Version')
-    );
+function add_gutenberg_editor_styles()
+{
+  wp_enqueue_style(
+    'theme-editor-styles',
+    get_theme_file_uri('editor-style.css'),
+    array(),
+    wp_get_theme()->get('Version')
+  );
 }
 add_action('enqueue_block_editor_assets', 'add_gutenberg_editor_styles');
 
 /**
  * Add custom styles specifically for ACF WYSIWYG fields
  */
-function acf_wysiwyg_editor_styles($toolbars) {
-    // Add custom CSS for ACF WYSIWYG fields
-    add_action('admin_head', function() {
-        echo '<style>
+function acf_wysiwyg_editor_styles($toolbars)
+{
+  // Add custom CSS for ACF WYSIWYG fields
+  add_action('admin_head', function () {
+    echo '<style>
         /* ACF WYSIWYG Field List Styles */
         .acf-editor-wrap .mce-content-body ul {
             list-style-type: disc !important;
@@ -94,8 +96,8 @@ function acf_wysiwyg_editor_styles($toolbars) {
             margin: 0.5em 0 !important;
         }
         </style>';
-    });
-    
-    return $toolbars;
+  });
+
+  return $toolbars;
 }
 add_filter('acf/fields/wysiwyg/toolbars', 'acf_wysiwyg_editor_styles');
