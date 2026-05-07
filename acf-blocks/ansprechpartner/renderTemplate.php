6c2ac032ffe052c5a3611ee175dc8bd358807741
Thomas Pondelek <thomas.pondelek@page-effect.com>
2026-05-07 19:29:09 +0200

    überarbeitung teamsektion

--- acf-blocks/ansprechpartner/renderTemplate.php
+++ acf-blocks/ansprechpartner/renderTemplate.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Block template file: renderTemplate.php
  *
@@ -12,81 +13,100 @@
 
 // Create id attribute allowing for custom "anchor" value.
 $id = 'ansprechpartner-' . $block['id'];
-if ( ! empty($block['anchor'] ) ) {
-    $id = $block['anchor'];
+if (! empty($block['anchor'])) {
+  $id = $block['anchor'];
 }
 
 // Create class attribute allowing for custom "className" and "align" values.
 $classes = 'ansprechpartner';
-if ( ! empty( $block['className'] ) ) {
-    $classes .= ' ' . $block['className'];
+if (! empty($block['className'])) {
+  $classes .= ' ' . $block['className'];
 }
-if ( ! empty( $block['align'] ) ) {
-    $classes .= ' align' . $block['align'];
+if (! empty($block['align'])) {
+  $classes .= ' align' . $block['align'];
 }
 ?>
 
-
-<?php
-$headline = get_field('headline');
-$subheadline = get_field('subheadline');
-$ansprechpartner_button = get_field('button');
-$bild = get_field( 'bild' );
-$beschreibung = get_field( 'beschreibung' );
-
-if (empty($bild)) {
-    $classes .= ' no-image';
-} else {
-    $classes .= ' has-image';
-}
-
-?>
 
 <section id="<?php echo esc_attr($id); ?>" class="module <?php echo esc_attr($classes); ?>">
- 
 
-    <div class="ansprechpartner__blur" aria-hidden="true"></div>
+  <div class="container">
+    <?php $headline_2 = get_field('sektions-uberschrift'); ?>
+    <?php if ($headline_2): ?>
+
+      <h2 class="headline headline-2">
+        <?= wp_kses_post($headline_2) ?>
+      </h2>
+    <?php endif; ?>
+    <div class="ansprechpartner group wrapper">
+      <?php if (have_rows('ansprechpartner_group')) : ?>
+        <?php while (have_rows('ansprechpartner_group')) : the_row(); ?>
+
+          <?php if (have_rows('ansprechpartner_single')) : ?>
+
+            <?php while (have_rows('ansprechpartner_single')) : the_row(); ?>
+
+              <!-- <div class="ansprechpartner__blur" aria-hidden="true"></div> -->
 
 
-    <div class="ansprechpartner__overlay container">
-      <div class="image">
-        
-	<?php if ( $bild ) : ?>
-		<img src="<?php echo esc_url( $bild['url'] ); ?>" alt="<?php echo esc_attr( $bild['alt'] ); ?>" />
-		<?php if ( !empty( $bild['caption'] ) ) : ?>
-			<figcaption class="image-caption"><?php echo wp_kses_post( $bild['caption'] ); ?></figcaption>
-		<?php endif; ?>
-	<?php endif; ?>
-      </div>
-        <div class="ansprechpartner__content text-wrapper">
-            <?php if ($headline): ?>
-            <h3 class="headline headline-3">
-                <?= wp_kses_post($headline) ?>
-            </h3>
-            <?php endif; ?>
+              <div class="ansprechpartner__overlay">
+                <?php
+                $headline_3 = get_sub_field('headline_3');
+                $subheadline = get_sub_field('subheadline');
+                $ansprechpartner_button = get_sub_field('button');
+                $bild = get_sub_field('bild');
+                $beschreibung = get_sub_field('beschreibung');
 
-            <?php if ($subheadline): ?>
-            <h4 class="headline headline-4 subheadline">
-                <?= wp_kses_post($subheadline) ?>
-            </h4>
-            <?php endif; ?>
-            <?php if ($beschreibung): ?>
-            <p class="beschreibung">
-                <?= wp_kses_post($beschreibung) ?>
-            </p>
-            <?php endif; ?>
+                if ($bild) : ?>
 
-            <?php if ($ansprechpartner_button): ?>
-            <div class="button-wrapper" role="group" aria-label="Actions">
-                <a class="btn btn--primary" href="<?php echo esc_url($ansprechpartner_button['url']); ?>"
-                    target="<?php echo esc_attr($ansprechpartner_button['target']); ?>"
-                    aria-describedby="<?php echo esc_html($ansprechpartner_button['title']); ?>">
-                    <span class="btn__text"><?php echo esc_html($ansprechpartner_button['title']); ?></span>
-                </a>
-                <span id="<?php echo esc_html($ansprechpartner_button['title']); ?>" class="sr-only">(Description for screen
-                    readers)</span>
-            </div>
-            <?php endif; ?>
-        </div>
+                  <div class="image">
+
+                    <img src="<?php echo esc_url($bild['url']); ?>" alt="<?php echo esc_attr($bild['alt']); ?>" />
+                    <?php if (!empty($bild['caption'])) : ?>
+                      <figcaption class="image-caption"><?php echo wp_kses_post($bild['caption']); ?></figcaption>
+                    <?php endif; ?>
+                  </div>
+                <?php endif; ?>
+
+                <div class="ansprechpartner__content text-wrapper">
+                  <?php if ($headline_3): ?>
+                    <h3 class="headline headline-3">
+                      <?= wp_kses_post($headline_3) ?>
+                    </h3>
+                  <?php endif; ?>
+
+                  <?php if ($subheadline): ?>
+                    <h4 class="headline headline-4 subheadline">
+                      <?= wp_kses_post($subheadline) ?>
+                    </h4>
+                  <?php endif; ?>
+                  <?php if ($beschreibung): ?>
+                    <p class="beschreibung">
+                      <?= wp_kses_post($beschreibung) ?>
+                    </p>
+                  <?php endif; ?>
+
+                  <?php if ($ansprechpartner_button): ?>
+                    <div class="button-wrapper" role="group" aria-label="Actions">
+                      <a class="btn btn--primary" href="<?php echo esc_url($ansprechpartner_button['url']); ?>"
+                        target="<?php echo esc_attr($ansprechpartner_button['target']); ?>"
+                        aria-describedby="<?php echo esc_html($ansprechpartner_button['title']); ?>">
+                        <span class="btn__text"><?php echo esc_html($ansprechpartner_button['title']); ?></span>
+                      </a>
+                      <span id="<?php echo esc_html($ansprechpartner_button['title']); ?>" class="sr-only">(Description for screen
+                        readers)</span>
+                    </div>
+                  <?php endif; ?>
+                </div>
+              </div>
+            <?php endwhile; ?>
+          <?php endif; ?>
+        <?php endwhile; ?>
+      <?php else : ?>
+        <?php // No rows found 
+        ?>
+      <?php endif; ?>
     </div>
+
+  </div>
 </section>
